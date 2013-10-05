e7bd3c534cbaddc8c2939b948c2e861c4c6f751d
Alan deLevie <adelevie@gmail.com>
2013-10-05 16:01:46 -0400

    modulizing

---
+++ test/basic.js
@@ -0,0 +1,23 @@
+citationLinker = require('../lib/citation-linker')
+
+var pagedown = require("pagedown");
+require("citation");
+var safeConverter = pagedown.getSanitizingConverter();
+
+citationLinker.addCitationLinkerToConverter(safeConverter);
+
+var text = 
+  [
+    '- a list',
+    '- of things',
+    '- and stuff',
+    '',
+    'foo',
+    'apples',
+    'pursuant to 5 U.S.C. 552(a)(1)(E) and',
+    'also pursuant to 47 C.F.R. Part 15',
+    'and something else from: D.C. Official Code §§ 38-2602(b)(11)'
+  ].join('\n');
+
+
+console.log(safeConverter.makeHtml(text));
