fd171b2d167c0050176b44a7b04f55ee5904e1fb
Alan deLevie <adelevie@gmail.com>
2013-10-05 15:40:59 -0400

    added links for dc code

--- main.js
+++ main.js
@@ -30,10 +30,29 @@ function makeCfrUrl(citation) {
   return "http://www.law.cornell.edu/cfr/text/" + title + "/" + section;
 }
 
+function makeDcCodeUrl(citation) {
+  console.log(citation);
+
+  /*
+  dc_code: 
+   { title: '3',
+     section: '1201.01',
+     subsections: [],
+     id: 'dc-code/3/1201.01',
+     section_id: 'dc-code/3/1201.01' } }
+  */
+  var dc_code = citation.dc_code;
+  var title = dc_code.title;
+  var section = dc_code.section;
+  return "http://dccode.org/browser/#/" + title + "/" + title + "-" + section;
+}
+
 function makeUrl(citation) {
   if (citation.type === "usc") { return makeUsCodeUrl(citation); }
   if (citation.type === "cfr") { return makeCfrUrl(citation); }
+  if (citation.type === "dc_code") { return makeDcCodeUrl(citation); }
 
+  // if no match, silently default to the plain text
   return citation.match;
 }
 
@@ -41,18 +60,18 @@ safeConverter.hooks.chain("preConversion", function (text) {
 
   var citations = Citation.find(text).citations;
 
+  /*
   if (citations.length === 0) {
     console.log("no citations found");
   } else {
     console.log("found a cite!");
   }
+  */
 
   var newText = ""
   for (var i=0,len=citations.length; i<len; i++) { 
-    console.log("replacing...");
     var match = citations[i].match;
     text = text.replace(match, "[" + match + "]" + "(" + makeUrl(citations[i]) + ")");
-    //console.log(bar);
   }
 
   return text;
@@ -67,7 +86,8 @@ var text =
     'foo',
     'apples',
     'pursuant to 5 U.S.C. 552(a)(1)(E) and',
-    'also pursuant to 47 C.F.R. Part 15'
+    'also pursuant to 47 C.F.R. Part 15',
+    'and something else from: D.C. Official Code §§ 38-2602(b)(11)'
   ].join('\n');
 
 
